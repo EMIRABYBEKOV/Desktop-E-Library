47518e6f81c977ccc6b15d34d70ab0a11f696778
EMIRABYBEKOV <emir.abdybekov@bk.ru>
2024-06-06 22:36:18 +0200

    push my ooold project

---
+++ Elibrary/app/save_pasedimages.py
@@ -0,0 +1,74 @@
+import requests
+import psycopg2
+import urllib.request
+
+db_name = "library"
+db_user = "user"
+# db_password =
+
+postgres = psycopg2.connect(
+    dbname=f'{db_name}',
+    user=f'{db_user}',
+    # password=f'{db_password}',
+    host='localhost'
+)
+cursor = postgres.cursor()
+
+
+def t1():
+    cursor.execute("SELECT image, id FROM en_books")
+    images = cursor.fetchall()
+
+    for i in images:
+        image = i[0]
+        id = i[1]
+        URL = imagex
+        img = urllib.request.urlopen(URL).read()
+        out = open(f"img.{id}-", "wb")
+        out.write(img)
+        out.close
+    cursor.execute('SELECT image, name, author, year, price, many FROM ai_books')
+    info = cursor.fetchall()
+
+    for i in info:
+        image = i[0]
+        name = i[1]
+        author = i[2]
+        year = i[3]
+        price = i[4]
+        many = i[5]
+        cursor.execute(f"INSERT INTO ch_books(image, name, author, year, price, many) VALUES('{image}','{name}','{author}','{year}',{price},{many})")
+        postgres.commit()
+
+        
+def t2():
+    cursor.execute("SELECT id FROM en_books")
+    id = cursor.fetchall()
+
+    for i in id:
+        idd = 'images/img.' + f'{i[0]}' + '-'
+        cursor.execute(f"UPDATE en_books SET image = '{idd}' WHERE id = {i[0]}")
+        postgres.commit()
+
+def t3():
+    cursor.execute("SELECT image, name FROM en_books")
+    id = cursor.fetchall()
+
+    for i in id:
+        idd = 'https://' + f'{i[0]}'
+        cursor.execute(f"UPDATE en_books SET image = '{idd}' WHERE name= '{i[1]}'")
+        postgres.commit()
+        
+def t4():
+    cursor.execute('SELECT image, name, author, year, price, many FROM en_books')
+    info = cursor.fetchall()
+
+    for i in info:
+        image = i[0]
+        name = i[1]
+        author = i[2]
+        year = i[3]
+        price = i[4]
+        many = i[5]
+        cursor.execute(f"INSERT INTO ch_books(image, name, author, year, price, many) VALUES('{image}','{name}','{author}','{year}',{price},{many})")
+        postgres.commit()
